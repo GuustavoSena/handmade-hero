b27a271b75a276186c1cbb665ab956312ac9604f
Gustavo Sena <sena170104@gmail.com>
2025-08-22 21:49:52 -0300

    Allocating a Backbuffer

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -1,10 +1,61 @@
 #include <windows.h>
 
+#define internal static 
+#define local_persist static 
+#define global_variable static 
+
+global_variable bool Running;
+global_variable BITMAPINFO BitmapInfo;
+global_variable void *BitmapMemory;
+global_variable HBITMAP BitmapHandle;
+global_variable HDC BitmapDeviceContext;
+
+internal void
+Win32ResizeDIBSection(int Width, int Height)
+{
+    if(BitmapHandle){
+        DeleteObject(BitmapHandle);
+    }
+    
+    if(!BitmapDeviceContext){
+        BitmapDeviceContext = CreateCompatibleDC(0);
+    }
+
+    BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
+    BitmapInfo.bmiHeader.biWidth = Width;
+    BitmapInfo.bmiHeader.biHeight = Height;
+    BitmapInfo.bmiHeader.biPlanes = 1;
+    BitmapInfo.bmiHeader.biBitCount = 32;
+    BitmapInfo.bmiHeader.biCompression = BI_RGB;
+
+    BitmapHandle =  CreateDIBSection(
+    BitmapDeviceContext, &BitmapInfo,
+    DIB_RGB_COLORS,
+    &BitmapMemory,
+    0, 0);
+
+    ReleaseDC(0, BitmapDeviceContext);
+}
+
+internal void
+Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Width, int Height)
+{
+        StretchDIBits(
+        DeviceContext,
+        X, Y, Width, Height,
+        X, Y, Width, Height,
+        BitmapMemory,
+        &BitmapInfo,
+        DIB_RGB_COLORS, SRCCOPY
+);
+}
+
+
 LRESULT CALLBACK
-MainWindowCallback(HWND Window,
-                   UINT Message,
-                   WPARAM WParam,
-                   LPARAM LParam)
+Win32MainWindowCallback(HWND Window,
+                        UINT Message,
+                        WPARAM WParam,
+                        LPARAM LParam)
 {
     LRESULT Result = 0;
 
@@ -12,11 +63,16 @@ MainWindowCallback(HWND Window,
     {
         case WM_SIZE:
         {
-            OutputDebugStringA("WM_SIZE\n");
+            RECT ClientRect;
+            GetClientRect(Window, &ClientRect);
+            int Width = ClientRect.right - ClientRect.left;
+            int Height = ClientRect.bottom - ClientRect.top;
+            Win32ResizeDIBSection(Width, Height);
         } break;
 
         case WM_CLOSE:
         {
+            Running = false;
             OutputDebugStringA("WM_CLOSE\n");
         } break;
 
@@ -27,6 +83,7 @@ MainWindowCallback(HWND Window,
 
         case WM_DESTROY:
         {
+            Running = false;
             OutputDebugStringA("WM_DESTROY\n");
         } break;
 
@@ -37,23 +94,13 @@ MainWindowCallback(HWND Window,
             int X = Paint.rcPaint.left;
             int Y = Paint.rcPaint.top;
             int Width = Paint.rcPaint.right - Paint.rcPaint.left;
-            int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
-            static DWORD Operation = WHITENESS;            
-            PatBlt(DeviceContext, X, Y, Width, Height, Operation);
-            if(Operation == WHITENESS)
-            {
-                Operation = BLACKNESS;
-            }
-            else
-            {
-                Operation = WHITENESS;
-            }
+            int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;\
+            Win32UpdateWindow(DeviceContext, X, Y, Width, Height);
             EndPaint(Window, &Paint);
         } break;
 
         default:
         {
-//            OutputDebugStringA("default\n");
             Result = DefWindowProc(Window, Message, WParam, LParam);
         } break;
     }
@@ -69,7 +116,7 @@ WinMain(HINSTANCE Instance,
 {
     WNDCLASS WindowClass = {};
     
-    WindowClass.lpfnWndProc = MainWindowCallback;
+    WindowClass.lpfnWndProc = Win32MainWindowCallback;
     WindowClass.hInstance = Instance;
 //    WindowClass.hIcon;
     WindowClass.lpszClassName = "HandmadeHeroWindowClass";
@@ -92,7 +139,8 @@ WinMain(HINSTANCE Instance,
                 0);
         if(WindowHandle)
         {
-            for(;;)
+            Running = true;
+            while(Running)
             {
                 MSG Message;
                 BOOL MessageResult = GetMessageA(&Message, 0, 0, 0);
